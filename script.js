be54f1ed17eddf004ed57426b46c01d081cac058
GiorgiKemashvili <163575886+GiorgiKemashvili@users.noreply.github.com>
2024-04-29 12:19:55 +0400

    Added Task 1

---
+++ script.js
@@ -0,0 +1,7 @@
+// Task 1
+const numbersArray = [1, 2, 3, 4, 5];
+let sum = 0;
+for (let i = 0; i < numbersArray.length; i++) {
+  sum += numbersArray[i];
+}
+console.log("Sum of all elements:", sum);
