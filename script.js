1c233e708cb0d682db2a8f3974e9be42236ddbaa
GiorgiKemashvili <163575886+GiorgiKemashvili@users.noreply.github.com>
2024-04-29 12:23:07 +0400

    Added Task 6

--- script.js
+++ script.js
@@ -32,3 +32,31 @@ console.log("Elements of the array:");
 for (let i = 0; i < numbersArray2.length; i++) {
   console.log(numbersArray2[i]);
 }
+//Task 6
+const currentDay = new Date().getDay();
+
+switch (currentDay) {
+  case 0:
+    console.log("Sunday");
+    break;
+  case 1:
+    console.log("Monday");
+    break;
+  case 2:
+    console.log("Tuesday");
+    break;
+  case 3:
+    console.log("Wednesday");
+    break;
+  case 4:
+    console.log("Thursday");
+    break;
+  case 5:
+    console.log("Friday");
+    break;
+  case 6:
+    console.log("Saturday");
+    break;
+  default:
+    console.log("Invalid day!");
+}
